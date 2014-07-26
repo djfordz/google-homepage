7fdbc1cb62f4b4684e920df73744cda8eb605915
djfordz <djfordz@gmail.com>
2014-07-26 10:54:42 -0500

    Initial Build version 0.0.0

---
+++ css/gulp.js
@@ -0,0 +1,51 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var serve = require('gulp-serve');
+var express = require('express');
+
+//configure the server
+gulp.task('serve', function () {
+    server.listen(3000, "localhost", function (err) {
+        if (err) { return console.log(err); }
+    });
+});
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['serve', 'lint', 'sass', 'scripts', 'watch']);
